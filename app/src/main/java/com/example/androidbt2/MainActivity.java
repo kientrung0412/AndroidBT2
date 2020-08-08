ef897211005085ebb5e9bca70f00045fa70fcfa2
Hanabi <nguyenkientrung0412@gmail.com>
2020-08-08 19:55:37 +0700

    gần xong

---
+++ app/src/main/java/com/example/androidbt2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidbt2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ex1);
+    }
+}
