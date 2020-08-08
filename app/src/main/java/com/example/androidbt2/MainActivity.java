8f1d53f11bae6ea57bc824ae632e28901832098f
Hanabi <nguyenkientrung0412@gmail.com>
2020-08-08 23:28:09 +0700

    Done 99%

--- app/src/main/java/com/example/androidbt2/MainActivity.java
+++ app/src/main/java/com/example/androidbt2/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ex1);
+        setContentView(R.layout.ex4);
     }
 }
